6ed516ec26e8fc4303cd36fd4fc7049ba07007b6
faizyi <faizi13062000@gmail.com>
2025-03-06 14:07:53 +0300

    apollo server

--- server/server.js
+++ server/server.js
@@ -3,24 +3,42 @@ import { ApolloServer } from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4";
 import bodyParser from "body-parser";
 import cors from "cors";
+import axios from "axios";
 
 
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+        type User {
+        id: ID!
+        username: String!
+        email: String!
+        age: Int!
+        phone: String!
+        website: String!
+        }
         type Todo {
         id: ID!
-        title: String!
+        todo: String!
         completed: Boolean!
+        user: User
         }
         type Query {
         getTodos: [Todo] 
+        getTodosById(id: ID!): Todo
+        getAllUsers: [User]
+        getUserById(id: ID!): User
         }
         `,
         resolvers:{
-            Query : {
-                getTodos: () => [{id: 1, title: "nnnn", completed: false}]
+            Todo: {
+                user : async (todo) => (await axios.get(`https://dummyjson.com/users/${todo.id}`)).data
+            },
+            Query : {getTodos: async () => (await axios.get('https://dummyjson.com/todos')).data.todos,
+                    getTodosById: async (_, {id}) => (await axios.get(`https://dummyjson.com/todos/${id}`)).data,
+                    getAllUsers: async () => (await axios.get("https://dummyjson.com/users")).data.users,
+                    getUserById: async (_, {id}) => (await axios.get(`https://dummyjson.com/users/${id}`)).data,
             }
         }
     })
